edd2c72ddb06126c4f38888961fafcbb80c138aa
queszel <queszeledu@gmail.com>
2025-10-02 10:19:42 -0300

    Added ability to place ships, attack ships, and check if a gameboard has lost (no ships left)

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,6 +3,50 @@ export class Gameboard {
     this.gameboard = initializeGameboard();
   };
 
+  placeShipAt(x, y, ship, direction) {
+    if (this.gameboard[y][x] === "W") {
+      if (checkIfCanPlace(this.gameboard, x, y, ship, direction)) {
+        if (direction === "right") {
+          for (let i = 0; i < ship.length; i++) {
+            this.gameboard[y][x + i] = "S";
+          }
+        }
+        if (direction === "up") {
+          for (let i = 0; i < ship.length; i++) {
+            this.gameboard[y + i][x] = "S";
+          }
+        }
+      }
+    }
+  }
+
+  receiveAttack(x, y) {
+    if (this.gameboard[y][x] === "W") {
+      return "W";
+    }
+    else {
+      this.gameboard[y][x] = "H";
+      return "H";
+    }
+  }
+
+  hasLost() {
+    return checkLoss(this.gameboard);
+  }
+
+
+}
+
+function checkLoss(gameboard) {
+  let hasLost = true;
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (gameboard[i][j] === "S") {
+        hasLost = false;
+      }
+    }
+  }
+  return hasLost;
 }
 
 function initializeGameboard() {
@@ -16,22 +60,6 @@ function initializeGameboard() {
   return board;
 }
 
-export function placeShipAt(gameboard, x, y, ship, direction) {
-  if (gameboard[y][x] === "W") {
-    if (checkIfCanPlace(gameboard, x, y, ship, direction)) {
-      if (direction === "right") {
-        for (let i = 0; i < ship.length; i++) {
-          gameboard[y][x + i] = "S";
-        }
-      }
-      if (direction === "up") {
-        for (let i = 0; i < ship.length; i++) {
-          gameboard[y + i][x] = "S";
-        }
-      }
-    }
-  }
-}
 
 function checkIfCanPlace(gameboard, x, y, ship, direction) {
   let canPlace = false;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -11,8 +11,54 @@ describe("gameboard", () => {
   })
 
   it("should place a 'destroyer at [0,0], [1,0]", () => {
-    placeShipAt(gb1.gameboard, 0, 0, ship, "right");
+    gb1.placeShipAt(0, 0, ship, "right");
     expect(gb1.gameboard[0][0]).toBe("S");
     expect(gb1.gameboard[0][1]).toBe("S");
   });
+
+  it("should see if a destroyer placed at [0,0], [1,0] has empty adjacent tiles", () => {
+    gb1.placeShipAt(0, 0, ship, "right");
+    expect(gb1.gameboard[0][2]).toBe("W");
+    expect(gb1.gameboard[1][0]).toBe("W");
+    expect(gb1.gameboard[1][1]).toBe("W");
+  })
+
+  describe("attacking the gameboard", () => {
+
+    beforeEach(() => {
+      gb1.placeShipAt(4, 4, ship, "right");
+      gb1.receiveAttack(4, 4);
+    })
+
+    it("should remain 'W' when a received attack misses", () => {
+      expect(gb1.gameboard[6][5]).toBe("W");
+    })
+
+    it("should update the position on the board to 'H' if it's a hit", () => {
+      expect(gb1.gameboard[4][4]).toBe("H");
+    })
+
+    it("should be 'W' next to the adjacent hit area", () => {
+      expect(gb1.gameboard[5][4]).toBe("W");
+    })
+
+  })
+
+  describe("checking the game status", () => {
+
+    beforeEach(() => {
+      gb1.placeShipAt(4, 4, ship, "right");
+      gb1.receiveAttack(4, 4);
+    })
+
+    it("should be false if the player has not lost", () => {
+      expect(gb1.hasLost()).toBe(false);
+    })
+
+    it("should be true if the player has lost (no ships left on the board)", () => {
+      gb1.receiveAttack(5, 4);
+      expect(gb1.hasLost()).toBe(true);
+    })
+  })
+
 });
